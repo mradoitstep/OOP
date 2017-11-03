ee03cd53c38f0b58691f812e2f7b250539181634
CSharpFri <CSharpFri@CLASS-2-10>
2017-11-03 20:55:14 +0100

    feat(OOP) add class Dom and Chata

---
+++ OOP/Chata.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Chata : Dom
+    {
+        private string spolocenskaMiestnost;
+
+        public Chata(string nazov, string typ, string poschodie, int obytneMiestnsti, float uzitkovaPlocha, float zastavanaPlocha, string spolocenskaMiestnost) : base(nazov, typ, poschodie, obytneMiestnsti, uzitkovaPlocha, zastavanaPlocha)
+        {
+
+            this.spolocenskaMiestnost = spolocenskaMiestnost;
+        }
+
+
+
+        public void PopisChaty()
+        {
+            Console.WriteLine("Nazov chaty {0}", this.nazov);
+            Console.WriteLine("spolocenska miestnost: {0}", spolocenskaMiestnost);
+
+        }
+    }
+}
+
+
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -10,7 +10,23 @@ namespace OOP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ahojahoj");
+            Dom mojDom = new Dom("Aktivny 1", "aktivny", "nie", 3, 70.9f, 34.8f);
+            mojDom.PoskytniInformacie();
+            Console.WriteLine("Percento zastavanej plochy je {0}", mojDom.ZistiVyuzitelnostZastavanejPlochy());
+
+            Dom susedovDom = new Dom("Aktivny 1", "aktivny", "nie", 3, 78.9f, 44.8f);
+            susedovDom.PoskytniInformacie();
+            Console.WriteLine("Percento zastavanej plochy je {0}", susedovDom.ZistiVyuzitelnostZastavanejPlochy());
+
+            Chata chata = new Chata("chata 1", "pasivna", "10", 50, 188.5f, 65.5f, "ano a velka");
+            chata.PoskytniInformacie();
+            chata.PopisChaty();
+            Console.WriteLine("Percento zastavanej plochy je {0}", chata.ZistiVyuzitelnostZastavanejPlochy());
+
+
+
+            Console.ReadLine();
+
         }
     }
 }
